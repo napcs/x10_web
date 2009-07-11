e7c1584025147916f3286f6163e1d052f7f61dc8
Brian Hogan <brianhogan@napcs.com>
2009-07-10 22:24:41 -0500

    updated for Rails 2.3

--- vendor/plugins/x10/lib/x10.rb
+++ vendor/plugins/x10/lib/x10.rb
@@ -5,27 +5,20 @@ module Acts
     def self.included(base)
       base.extend Acts::X10::ClassMethods
     end
-    
-    # this module should contain your code that you want mixed in to the class.
-    # it's also a good place to put a macro that invokes your plugin.
+
     module ClassMethods
-      
-      # Adds class and instance methods to the model to demonstrate how an ActiveRecord plugin should work
+      # Adds methods to interact with an X10 ActiveHome controller
       #
-      #  class User < ActiveRecord::Base
-      #     acts_as_example
+      #  class Unit < ActiveRecord::Base
+      #     acts_as_x10_module
       #  end
       #
-      # This will generate the following instance methods
-      #  * to_example  : 
-      # This also generates the following class methods
-      #  * latest_five
       def acts_as_x10_module(options = {})
 	    
         class_eval <<-END
-		  ::AHCMD_PATH = options[:sdk_path] || "c:/ahome/ahcmd.exe"
+		      ::AHCMD_PATH = options[:sdk_path] || "c:/ahome/ahcmd.exe"
           include Acts::X10::InstanceMethods
-		  extend Acts::X10::SingletonMethods
+		      extend Acts::X10::SingletonMethods
         END
       end
       
@@ -37,7 +30,7 @@ module Acts
 
 	end
     
-    module InstanceMethods
+  module InstanceMethods
       
 	  def on
 	    cmd(self.code, "on")
@@ -56,9 +49,9 @@ module Acts
 	  end
 	  
 	  private
-        def	cmd(code, command, amount = nil)
+      def	cmd(code, command, amount = nil)
 	      `#{::AHCMD_PATH} sendplc #{code} #{command} #{amount}`
-		end
+		  end
       
     end
     
